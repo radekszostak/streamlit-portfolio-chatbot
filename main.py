6820e6139c23d49bedcf1ff20b46ae892bd78615
Radoslaw Szostak <radoslawszostak@gmail.com>
2024-02-03 16:17:14 +0100

    update main

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 
-@st.cache_resource
-def initialize():
-    client = OpenAI(model="gpt-3.5-turbo-instruct", api_key=st.secrets["OPENAI_API_KEY"], streaming=True)
+if "messages" not in st.session_state:
+    welcome_message = """Hi there! I am a chatbot that helps people to get information about Radek, a data scientist and machine learning engineer. What would you like to know?"""
+    st.session_state.messages = [{"role": "assistant", "content": welcome_message}]
+
+if "client" not in st.session_state:
+    st.session_state.client = OpenAI(model="gpt-3.5-turbo-instruct", api_key=st.secrets["OPENAI_API_KEY"], streaming=True)
+
+if "retriever" not in st.session_state:
     embeddings = OpenAIEmbeddings(openai_api_key=st.secrets['OPENAI_API_KEY'])
     vectordb = FAISS.load_local("faiss_db", embeddings)
-    retriever = vectordb.as_retriever()
+    st.session_state.retriever = vectordb.as_retriever()
+
+if "prompt_template" not in st.session_state:
     template = """You are a chatbot that helps people to get information about Radek Szostak. Radek is a data scientist and a machine learning engineer. He is always ready to help people. He is also a very good mentor.
     You have access to chat history in order to keep context of the conversation. You are also provided with additional information where you can search for information about Radek. Respond with single assistant message.
 
@@ -21,14 +28,7 @@ def initialize():
     {chat_context}
     assisstant:
     """
-    prompt_template = PromptTemplate(input_variables=['chat_context', 'doc_context'], template=template)
-    welcome_message = """Hi there! I am a chatbot that helps people to get information about Radek, a data scientist and machine learning engineer. What would you like to know?"""
-    st.session_state.messages = [{"role": "assistant", "content": welcome_message}]
-    st.session_state.client = client
-    st.session_state.retriever = retriever
-    st.session_state.prompt_template = prompt_template
-    
-initialize()
+    st.session_state.prompt_template = PromptTemplate(input_variables=['chat_context', 'doc_context'], template=template)
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -41,7 +41,6 @@ if prompt := st.chat_input("Type your message here..."):
     chat_context = '\n'.join([f'{m["role"]}: {m["content"]}' for m in st.session_state.messages])
     doc_context = "\n\n".join([str(item.metadata) + "\n" + item.page_content for item in st.session_state.retriever.get_relevant_documents(chat_context)])
 
-    print(chat_context)
     with st.chat_message("user"):
         st.markdown(prompt)
         
